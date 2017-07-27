427a8c1f8d2e566410d6dc6bed6589b4af09f372
root <root@varaprasad1.mylabserver.com>
2017-07-27 18:55:17 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["varaprasad2"] = { "site_title" => "Varas websites coming soon", "port" => 80, "domain" => "varaprasad2.mylabserver.com" }
+default["apache"]["sites"]["varaprasad2b"] = { "site_title" => "Varabs websites coming soon", "port" => 80, "domain" => "varaprasad2b.mylabserver.com" }
+default["apache"]["sites"]["varaprasad3"] = { "site_title" => "varaprasad3 website", "port" => 80, "domain" => "varaprasad3.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
